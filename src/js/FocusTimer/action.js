8abcf77bd93ad0da6a13dbfe78225795f894e536
ThalesFortes <thales_pro@outlook.com>
2024-05-26 21:26:35 -0300

    Função de pause

--- src/js/FocusTimer/action.js
+++ src/js/FocusTimer/action.js
@@ -2,13 +2,16 @@ import * as timer from "./timer.js"
 import state from "./state.js"
 
 export function startRunning(){
-    document.documentElement.classList.toggle('running')
-    state.isRunning = !state.isRunning
+    document.documentElement.classList.add('running')
+    document.documentElement.classList.remove('stop')
+    state.isRunning = true
     timer.countdow()
 }
 
 export function stopTimer(){
-    console.log('stopTimer')
+    document.documentElement.classList.remove('running')
+    document.documentElement.classList.add('stop')
+    state.isRunning = false
 }
 
 
